68803973b20c48de577de2eac7e4a7d8f07fc394
Yuhong Cheng <mabelyuhong0319@gmail.com>
2022-01-18 12:55:27 -0800

    update version to trigger build

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -388,7 +388,7 @@ class BeamModulePlugin implements Plugin<Project> {
 
     // Automatically use the official release version if we are performing a release
     // otherwise append '-SNAPSHOT'
-    project.version = '2.37.0.3'
+    project.version = '2.37.0.4'
 
     if (isLinkedin(project)) {
       project.ext.mavenGroupId = 'com.linkedin.beam'
