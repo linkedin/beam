6fc8bb9c3eeb752376eaaae329f947c0dd37b0b1
Xinyu Liu <xiliu@xiliu-mn1.linkedin.biz>
2023-01-24 10:51:22 -0800

    cat individual test results

--- run_tests.sh
+++ run_tests.sh
@@ -4,3 +4,15 @@
 
 echo "Show Test Result"
 cat ./runners/samza/job-server/build/reports/tests/validatesPortableRunnerEmbedded/index.html
+
+echo "SimplePushbackSideInputDoFnRunnerTest"
+cat ./runners/samza/job-server/build/reports/tests/validatesPortableRunnerEmbedded/classes/org.apache.beam.runners.core.SimplePushbackSideInputDoFnRunnerTest.html
+
+echo "WindowingTest"
+cat ./runners/samza/job-server/build/reports/tests/validatesPortableRunnerEmbedded/classes/org.apache.beam.sdk.transforms.windowing.WindowingTest.html
+
+echo "PCollectionRowTupleTest"
+cat ./runners/samza/job-server/build/reports/tests/validatesPortableRunnerEmbedded/classes/org.apache.beam.sdk.values.PCollectionRowTupleTest.html
+
+echo "PCollectionTupleTest"
+cat ./runners/samza/job-server/build/reports/tests/validatesPortableRunnerEmbedded/classes/org.apache.beam.sdk.values.PCollectionTupleTest.html
