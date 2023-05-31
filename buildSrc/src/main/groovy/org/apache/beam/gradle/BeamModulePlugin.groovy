100d06ee9da1bdfbac765e8ba0a5e91d90c31b08
Katie Liu <katiliu@linkedin.com>
2023-05-31 13:31:52 -0700

    Update versions to 200.39.1.3

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -398,7 +398,7 @@ class BeamModulePlugin implements Plugin<Project> {
 
     // Automatically use the official release version if we are performing a release
     // otherwise append '-SNAPSHOT'
-    project.version = '2.45.10'
+    project.version = '200.39.1.3'
     if (isLinkedin(project)) {
       project.ext.mavenGroupId = 'com.linkedin.beam'
     }
