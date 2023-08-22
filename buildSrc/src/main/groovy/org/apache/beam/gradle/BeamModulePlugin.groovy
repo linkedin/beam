e2c2c04e2cef4c4416f969c4c9f643802996bf19
Alan Zhang <alazhang@linkedin.com>
2023-08-22 16:56:05 -0700

    update version for test

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -398,7 +398,7 @@ class BeamModulePlugin implements Plugin<Project> {
 
     // Automatically use the official release version if we are performing a release
     // otherwise append '-SNAPSHOT'
-    project.version = '200.45.13.1'
+    project.version = '200.45.13.2'
     if (isLinkedin(project)) {
       project.ext.mavenGroupId = 'com.linkedin.beam'
     }
