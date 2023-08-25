b831adec9f31140566a865a05937e10620f2022d
Alan Zhang <alazhang@linkedin.com>
2023-08-24 22:08:24 -0700

    test version

--- build.gradle.kts
+++ build.gradle.kts
@@ -591,58 +591,34 @@ tasks.register("checkSetup") {
 //  }
 //}
 
-allprojects {
-
-  project.getAllTasks(true).forEach {projectTasks ->
-    if (projectTasks.key.path == ":auto-elr") {
-      projectTasks.value.forEach {task ->
-        println("==>" + task.path)
-        if (task.path == ":auto-elr:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository") {
-          task.mustRunAfter(":model:pipeline:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-          task.mustRunAfter(":runners:core-construction-java:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-          task.mustRunAfter(":runners:core-java:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-          task.mustRunAfter(":runners:java-fn-execution:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-          task.mustRunAfter(":runners:samza:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-          task.mustRunAfter(":runners:spark:2:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-          task.mustRunAfter(":runners:spark:3:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-          task.mustRunAfter(":runners:portability:java:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-          task.mustRunAfter(":sdks:java:core:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-          task.mustRunAfter(":sdks:java:extensions:join-library:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-          task.mustRunAfter(":sdks:java:extensions:sorter:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-          task.mustRunAfter(":sdks:java:extensions:sql:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-          task.mustRunAfter(":sdks:java:io:hadoop-common:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-          task.mustRunAfter(":sdks:java:io:hadoop-file-system:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-          task.mustRunAfter(":sdks:java:io:hadoop-format:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-          task.mustRunAfter(":sdks:java:io:kafka:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-        }
-      }
-    }
-  }
-//  tasks.register("auto-elr-task") {
-//    println("==> setup dependsOn")
-//    dependsOn(":beam:model:pipeline:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-////  dependsOn(":runners:core-construction-java:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-////  dependsOn(":runners:core-java:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-////  dependsOn(":runners:java-fn-execution:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-////  dependsOn(":runners:samza:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-////  dependsOn(":runners:spark:2:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-////  dependsOn(":runners:spark:3:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-////  dependsOn(":runners:portability:java:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-////  dependsOn(":sdks:java:core:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-////  dependsOn(":sdks:java:extensions:join-library:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-////  dependsOn(":sdks:java:extensions:sorter:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-////  dependsOn(":sdks:java:extensions:sql:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-////  dependsOn(":sdks:java:io:hadoop-common:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-////  dependsOn(":sdks:java:io:hadoop-file-system:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-////  dependsOn(":sdks:java:io:hadoop-format:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-////  dependsOn(":sdks:java:io:kafka:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
-//
-//    doLast {
-//      println("==> setup dependsOn for auto-elr itself")
-//      dependsOn(":beam:auto-elr:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
+//allprojects {
+//  project.getAllTasks(true).forEach {projectTasks ->
+//    if (projectTasks.key.path == ":auto-elr") {
+//      projectTasks.value.forEach {task ->
+//        println("==>" + task.path)
+//        if (task.path == ":auto-elr:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository") {
+//          task.mustRunAfter(":model:pipeline:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
+//          task.mustRunAfter(":runners:core-construction-java:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
+//          task.mustRunAfter(":runners:core-java:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
+//          task.mustRunAfter(":runners:java-fn-execution:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
+//          task.mustRunAfter(":runners:samza:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
+//          task.mustRunAfter(":runners:spark:2:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
+//          task.mustRunAfter(":runners:spark:3:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
+//          task.mustRunAfter(":runners:portability:java:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
+//          task.mustRunAfter(":sdks:java:core:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
+//          task.mustRunAfter(":sdks:java:extensions:join-library:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
+//          task.mustRunAfter(":sdks:java:extensions:sorter:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
+//          task.mustRunAfter(":sdks:java:extensions:sql:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
+//          task.mustRunAfter(":sdks:java:io:hadoop-common:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
+//          task.mustRunAfter(":sdks:java:io:hadoop-file-system:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
+//          task.mustRunAfter(":sdks:java:io:hadoop-format:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
+//          task.mustRunAfter(":sdks:java:io:kafka:publishMavenJavaPublicationToLinkedin.jfrog.httpsRepository")
+//        }
+//      }
 //    }
 //  }
-}
+//}
+
 
 // Configure the release plugin to do only local work; the release manager determines what, if
 // anything, to push. On failure, the release manager can reset the branch without pushing.
--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -398,7 +398,7 @@ class BeamModulePlugin implements Plugin<Project> {
 
     // Automatically use the official release version if we are performing a release
     // otherwise append '-SNAPSHOT'
-    project.version = '200.45.13.7'
+    project.version = '200.45.13.8'
     if (isLinkedin(project)) {
       project.ext.mavenGroupId = 'com.linkedin.beam'
     }
