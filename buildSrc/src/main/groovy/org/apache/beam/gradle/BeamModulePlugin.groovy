2a537120e91b964947e514f968a4d8de59c20651
Ajo Thomas <ajo.thomas24@gmail.com>
2024-02-16 15:38:40 -0800

    Update version again

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -398,7 +398,7 @@ class BeamModulePlugin implements Plugin<Project> {
 
     // Automatically use the official release version if we are performing a release
     // otherwise append '-SNAPSHOT'
-    project.version = '200.45.18.1'
+    project.version = '200.45.18.2'
     if (isLinkedin(project)) {
       project.ext.mavenGroupId = 'com.linkedin.beam'
     }
