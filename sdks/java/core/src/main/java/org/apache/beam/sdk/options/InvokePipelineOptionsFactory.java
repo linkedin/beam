63035f6995b05bfa6eeebead1431cf0596ccb03b
Ajo Thomas <ajo.thomas24@gmail.com>
2024-02-27 18:25:32 -0800

    Revert "add comments"
    This reverts commit 7a9229783627f48df25ead07ac7ae4fe515cc4e6.

--- sdks/java/core/src/main/java/org/apache/beam/sdk/options/InvokePipelineOptionsFactory.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/options/InvokePipelineOptionsFactory.java
@@ -8,9 +8,6 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 
 
 @SuppressWarnings("rawtypes")
-/**
- * Interface to support offspring wire-in for Li
- */
 public interface InvokePipelineOptionsFactory<T> {
   T getPipelineOptions(T pipelineOptions, Class<T> clazz);
 
