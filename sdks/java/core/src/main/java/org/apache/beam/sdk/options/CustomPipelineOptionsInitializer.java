8c0d5ef31636c8d6824bc404c9e3bfeb1cf5969f
Ajo Thomas <ajo.thomas24@gmail.com>
2024-02-27 18:25:15 -0800

    Revert "address comments"
    This reverts commit 33df8688af9ca59a218b33d47e943580da45bdb2.

--- sdks/java/core/src/main/java/org/apache/beam/sdk/options/CustomPipelineOptionsInitializer.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/options/CustomPipelineOptionsInitializer.java
@@ -8,9 +8,8 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 
 
 /**
- * Interface to support offspring wire-in for Li: if input class is a
- * customized pipelineOptions created by an offspring factory,
- * the pipelineOptions will be initialized.
+ * Interface to support offspring wire-in for Li: if input class meets some requirements,
+ * the customized pipelineOptions will be initialized.
  *
  */
 @SuppressWarnings("rawtypes")
@@ -22,7 +21,7 @@ public interface CustomPipelineOptionsInitializer<T> {
   }
 
 
-  static @Initialized @Nullable CustomPipelineOptionsInitializer get() {
+  static @Initialized @Nullable CustomPipelineOptionsInitializer getFactory() {
     final Iterator<CustomPipelineOptionsInitializer.Registrar>
         factories = ServiceLoader.load(CustomPipelineOptionsInitializer.Registrar.class).iterator();
     return factories.hasNext() ? Iterators.getOnlyElement(factories).create() : null;
