48b2350180be699260f1a47283f9b3eb022fb20b
Ajo Thomas <ajo.thomas24@gmail.com>
2024-02-27 18:26:11 -0800

    Revert "Invoke RunnerPipelineOptionsFactory by checking stack trace (#105)"
    This reverts commit 661324bbc0f65f5d387fcbfb29ee36ccbf05a0f9.

--- sdks/java/core/src/main/java/org/apache/beam/sdk/options/PipelineOptionsFactory.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/options/PipelineOptionsFactory.java
@@ -227,6 +227,8 @@ public class PipelineOptionsFactory {
     private final boolean strictParsing;
     private final boolean isCli;
 
+    private static final ThreadLocal<Boolean> USE_RUNNER_FACTORY =
+        ThreadLocal.withInitial(() -> Boolean.TRUE);
     private static final RunnerPipelineOptionsFactory RUNNER_PIPELINE_OPTIONS_FACTORY =
         RunnerPipelineOptionsFactory.getFactory();
 
@@ -323,11 +325,17 @@ public class PipelineOptionsFactory {
      */
     public <T extends PipelineOptions> T as(Class<T> klass) {
       // LinkedIn specific logic to create PipelineOptions from Runner
-      if (RUNNER_PIPELINE_OPTIONS_FACTORY != null
-          // RunnerPipelineOptionsFactory should NOT be called recursively
-          && RunnerPipelineOptionsFactory.findFactoryCaller() == null) {
+      if (USE_RUNNER_FACTORY.get() && RUNNER_PIPELINE_OPTIONS_FACTORY != null) {
+        // RunnerPipelineOptionsFactory should NOT be used recursively
+        USE_RUNNER_FACTORY.set(false);
 
-        return RUNNER_PIPELINE_OPTIONS_FACTORY.getPipelineOptions(args, klass);
+        T options;
+        try {
+          options = RUNNER_PIPELINE_OPTIONS_FACTORY.getPipelineOptions(args, klass);
+        } finally {
+          USE_RUNNER_FACTORY.set(true);
+        }
+        return options;
       }
 
       Map<String, Object> initialOptions = Maps.newHashMap();
