31d38abd815c9e078dd18f7e565e00456a76b098
Ajo Thomas <ajo.thomas24@gmail.com>
2024-02-27 18:26:13 -0800

    Revert "Update Beam version (#104)"
    This reverts commit 5b63101fe8a1a91c9001050a0ca9d3ab1361dc98.

--- runners/flink/src/main/java/org/apache/beam/runners/flink/FlinkStreamingPortablePipelineTranslator.java
+++ runners/flink/src/main/java/org/apache/beam/runners/flink/FlinkStreamingPortablePipelineTranslator.java
@@ -418,8 +418,7 @@ public class FlinkStreamingPortablePipelineTranslator
     DataStream<WindowedValue<KeyedWorkItem<K, V>>> workItemStream =
         inputDataStream
             .flatMap(
-                new FlinkStreamingTransformTranslators.ToKeyedWorkItem(
-                    context.getPipelineOptions()))
+                new FlinkStreamingTransformTranslators.ToKeyedWorkItem(context.getPipelineOptions()))
             .returns(workItemTypeInfo)
             .name("ToKeyedWorkItem");
 
