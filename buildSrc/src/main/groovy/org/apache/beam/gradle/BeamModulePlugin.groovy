b15c592ba2b64198710d4176c98057137cc1242c
Ajo Thomas <ajo.thomas24@gmail.com>
2024-02-29 02:47:38 -0800

    Update version to 200.45.12.5

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -398,7 +398,7 @@ class BeamModulePlugin implements Plugin<Project> {
 
     // Automatically use the official release version if we are performing a release
     // otherwise append '-SNAPSHOT'
-    project.version = '200.45.12.4'
+    project.version = '200.45.12.5'
     if (isLinkedin(project)) {
       project.ext.mavenGroupId = 'com.linkedin.beam'
     }
--- sdks/python/apache_beam/version.py
+++ sdks/python/apache_beam/version.py
@@ -17,4 +17,4 @@
 
 """Apache Beam SDK version information and utilities."""
 
-__version__ = '200.45.12.4'
+__version__ = '200.45.12.5'
